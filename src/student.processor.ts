6abcdd6127fa765068ed4bb00fd53c71045e93fc
Dimansha Wijebandara <dimansha.wijebandara@gmail.com>
2021-09-19 14:15:22 +0530

    sockets added

--- src/student.processor.ts
+++ src/student.processor.ts
@@ -1,16 +1,28 @@
-import { OnGlobalQueueCompleted, OnQueueActive, Process, Processor } from "@nestjs/bull";
+import { OnGlobalQueueCompleted, OnQueueActive, OnQueueCompleted, Process, Processor } from "@nestjs/bull";
 import { Logger } from "@nestjs/common";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Job } from "bull";
+import { Socket } from "dgram";
 import { AppService } from './app.service';
 
+
+@WebSocketGateway({cors: {origin: 'http://localhost:4200'}})
 @Processor('stQueue')
 export class StudentProcessor {
     constructor(private readonly appService: AppService, ){}
     private readonly logger = new Logger(StudentProcessor.name);
 
+    @WebSocketServer()
+    socket;
+
     @Process('saveStudent')
     async saveStudent(job: Job){
         await this.appService.saveStudent(job.data);
+    }
+
+    @SubscribeMessage('events')
+    handleEvent(@MessageBody() data: string):string {
+        return data;
     }
 
     @OnQueueActive()
@@ -20,9 +32,16 @@ export class StudentProcessor {
         );
     }
 
+    // @OnQueueCompleted()
+    // async onQueueCompleted(jobId: number, result: any){
+    //     this.socket.emit('events', {name: 'Nest'});
+    //}
+
     @OnGlobalQueueCompleted()
     async onGlobalCompleted(jobId: number, result: any){
         console.log('(Global) on completed: job ', jobId, ' -> result: ', result);
+        ////////////////
+        this.socket.emit('events', {name: 'Nest'});
     }
     
 }
